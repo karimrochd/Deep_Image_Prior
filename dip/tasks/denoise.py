cc521079704b4a4b8859700657aaaba3886d1fe3
karimrochd <rochdkarim02@gmail.com>
2025-11-16 15:08:49 +0100

    uploading

--- dip/tasks/denoise.py
+++
@@ -1,8 +0,0 @@
-import torch
-import torch.nn.functional as F
-from .common import BaseTask
-
-class DenoiseTask(BaseTask):
-    name = 'denoise'
-    def data_loss(self, y, obs, **kw):
-        return F.mse_loss(y, obs)
